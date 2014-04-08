6c30572f889ed4f85324c092ed59efafdc5e5d52
Cristian Georgescu <georgescu.cristi@gmail.com>
2014-04-08 16:06:52 +0300

    Initial commit refinery 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rash::Application.config.secret_key_base = '315647a27aec075050b5b1024d968689239cb44b1dd742dbfa5ad99f9ef1b750b09316e7a7ceba77167c97c426190db728b4469a74b4afd6a4e7c3e9c5da0887'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rash::Application.config.session_store :cookie_store, key: '_rash_session'
