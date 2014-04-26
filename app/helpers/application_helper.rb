8a509dd0624b1992733e959e3aa2150942fb4859
Cristian Georgescu <georgescu.cristi@gmail.com>
2014-04-26 17:28:18 +0300

    fixed header menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def navigation_menu
     presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
-    presenter.dom_id = "navbar-main"
-    presenter.css = "navbar-collapse collapse"
-    presenter.menu_tag = :div
-    presenter.list_tag_css = "nav navbar-nav navbar-right"
-    presenter.selected_css = ""
+    presenter.dom_id = ""
+    presenter.css = "nav-main"
+    presenter.menu_tag = :nav
+    presenter.list_tag_css = "nav nav-pills nav-main scroll-menu"
+    presenter.selected_css = "active"
     presenter.first_css = ""
     presenter.last_css = ""
     presenter.max_depth = 0 # prevents dropdown menus, which don't render correctly
